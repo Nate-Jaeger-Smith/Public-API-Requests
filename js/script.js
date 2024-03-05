7776cb7f068f7ccc27a1e8e5aaf8a469d3bf1029
Nate Jaeger-Smith <157942163+Nate-Jaeger-Smith@users.noreply.github.com>
2024-03-04 20:00:37 -0600

    Adds eventListener for modal close button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const gallery = document.getElementById('gallery');
 let employees;
+
 async function getEmployees(){
     try {
         const response = await fetch('https://randomuser.me/api/?results=12');
@@ -38,26 +39,28 @@ showEmployees();
 
 function showModal(employee){
     const modalDiv = document.createElement('div');
+    const DOB = new Date(employee.dob.date);
     modalDiv.classList.add('modal-container');
     
     modalDiv.innerHTML = `
-        <div class="modal-container">
-            <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                <div class="modal-info-container">
-                    <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
-                    <p class="modal-text">${employee.email}</p>
-                    <p class="modal-text cap">${employee.location.city}</p>
-                    <hr>
-                    <p class="modal-text">${employee.cell}</p>
-                    <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                    <p class="modal-text">Birthday: ${new Date(employee.dob.date)}</p>
-                </div>
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                <p class="modal-text">${employee.email}</p>
+                <p class="modal-text cap">${employee.location.city}</p>
+                <hr>
+                <p class="modal-text">${employee.cell}</p>
+                <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                <p class="modal-text">Birthday: ${DOB.getMonth()} / ${DOB.getDate()} / ${DOB.getFullYear()}</p>
             </div>
         </div>
     `;
     document.body.appendChild(modalDiv);
+
+    const closeButton = document.querySelector('#modal-close-btn');
+    closeButton.addEventListener('click',() => modalDiv.remove());
 }
 
 gallery.addEventListener('click', e => {
