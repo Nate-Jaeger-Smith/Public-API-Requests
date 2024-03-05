7a44f3b37f8d7ebe364a71f56e0ec34db53684d0
Nate Jaeger-Smith <157942163+Nate-Jaeger-Smith@users.noreply.github.com>
2024-03-04 18:24:24 -0600

    Adds event on gallery & creates showModal()
    Modal is appended to body, needs more info displayed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const gallery = document.getElementById('gallery');
-
+let employees;
 async function getEmployees(){
     try {
         const response = await fetch('https://randomuser.me/api/?results=12');
@@ -16,7 +16,7 @@ async function getEmployees(){
 }
 
 async function showEmployees(){
-    const employees = await getEmployees();
+    employees = await getEmployees();
 
     employees.map( employee => {
         let html = `
@@ -35,3 +35,34 @@ async function showEmployees(){
     });
 }
 showEmployees();
+
+function showModal(employee){
+    const modalDiv = document.createElement('div');
+    modalDiv.classList.add('modal-container');
+    
+    modalDiv.innerHTML = `
+        <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                    <p class="modal-text">${employee.email}</p>
+                    <p class="modal-text cap">${employee.location.city}</p>
+                    <hr>
+                    <p class="modal-text">${employee.cell}</p>
+                    <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                    <p class="modal-text">Birthday: ${new Date(employee.dob.date)}</p>
+                </div>
+            </div>
+        </div>
+    `;
+    document.body.appendChild(modalDiv);
+}
+
+gallery.addEventListener('click', e => {
+    const targetcard = e.target.closest('.card');
+    const targetName = targetcard.querySelector('#name').textContent;
+    const foundEmployee = employees.find( employee => `${employee.name.first} ${employee.name.last}` === targetName);
+    showModal(foundEmployee);
+});
